d5d2d03f553ff82fcd2a0b4ed1004366d9e8dce1
shirishdh15 <121274610+shirishdh15@users.noreply.github.com>
2023-05-27 17:37:39 +0530

    Add files via upload

---
+++ Contact.java
@@ -0,0 +1,12 @@
+package practiceProblems;
+
+public class Contact {
+    public String firstName;
+    public String LastName;
+    public String address;
+    public String city;
+    public String state;
+    public String Zip;
+    public String phoneNumber;
+    public String email;
+}
